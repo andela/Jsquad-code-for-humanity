b3d0e503e26b526be28d925f312ac283724cb817
Natalie <natalie.elizabeth@andela.com>
2017-04-25 14:48:19 +0300

    <Feature>: #143410121 JWT Login Authentication
    Signed-off-by: Natalie <natalie.elizabeth@andela.com>

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,21 +1,20 @@
 /**
  * Module dependencies.
  */
-var mongoose = require('mongoose'),
-  User = mongoose.model('User');
-var avatars = require('./avatars').all();
-var jwt = require('jsonwebtoken');
+const mongoose = require('mongoose'),
 
-/**
- * Auth callback
- */
-exports.authCallback = function (req, res, next) {
+  User = mongoose.model('User');
+const avatars = require('./avatars').all();
+const jwt = require('jsonwebtoken');
+
+const secret = 'secretoverhere';
+
+// Auth callback
+exports.authCallback = function (req, res) {
   res.redirect('/chooseavatars');
 };
 
-/**
- * Show login form
- */
+// Show login form
 exports.signin = function (req, res) {
   if (!req.user) {
     res.redirect('/#!/signin?error=invalid');
@@ -24,9 +23,8 @@ exports.signin = function (req, res) {
   }
 };
 
-/**
- * Show sign up form
- */
+// Show sign up form
+
 exports.signup = function (req, res) {
   if (!req.user) {
     res.redirect('/#!/signup');
@@ -35,26 +33,20 @@ exports.signup = function (req, res) {
   }
 };
 
-/**
- * Logout
- */
+// Logout
+
 exports.signout = function (req, res) {
   req.logout();
   res.redirect('/');
 };
 
-/**
- * Session
- */
+// Session
 exports.session = function (req, res) {
   res.redirect('/');
 };
 
-/** 
- * Check avatar - Confirm if the user who logged in via passport
- * already has an avatar. If they don't have one, redirect them
- * to our Choose an Avatar page.
- */
+// Check avatar - Confirm if the user who logged in via passport already has an avatar.
+
 exports.checkAvatar = function (req, res) {
   if (req.user && req.user._id) {
     User.findOne({
@@ -71,19 +63,17 @@ exports.checkAvatar = function (req, res) {
     // If user doesn't even exist, redirect to /
     res.redirect('/');
   }
-
 };
 
-/**
- * Create user
- */
-exports.create = function (req, res) {
+// Create user
+
+exports.create = function (req, res, next) {
   if (req.body.name && req.body.password && req.body.email) {
     User.findOne({
       email: req.body.email
     }).exec(function (err, existingUser) {
       if (!existingUser) {
-        var user = new User(req.body);
+        const user = new User(req.body);
         // Switch the user's avatar index to an actual avatar url
         user.avatar = avatars[user.avatar];
         user.provider = 'local';
@@ -91,7 +81,7 @@ exports.create = function (req, res) {
           if (err) {
             return res.render('/#!/signup?error=unknown', {
               errors: err.errors,
-              user: user
+              user // thomas
             });
           }
           req.logIn(user, function (err) {
@@ -108,9 +98,8 @@ exports.create = function (req, res) {
   }
 };
 
-/**
- * Assign avatar to user
- */
+// Assign avatar to user
+
 exports.avatars = function (req, res) {
   // Update the current user's profile to include the avatar choice they've made
   if (req.user && req.user._id && req.body.avatar !== undefined &&
@@ -135,14 +124,14 @@ exports.addDonation = function (req, res) {
       })
         .exec(function (err, user) {
           // Confirm that this object hasn't already been entered
-          var duplicate = false;
-          for (var i = 0; i < user.donations.length; i++) {
+          let duplicate = false;
+          for (let i = 0; i < user.donations.length; i += 1) {
             if (user.donations[i].crowdrise_donation_id === req.body.crowdrise_donation_id) {
               duplicate = true;
             }
           }
           if (!duplicate) {
-            console.log('Validated donation');
+            // console.log('Validated donation');
             user.donations.push(req.body);
             user.premium = 1;
             user.save();
@@ -153,33 +142,26 @@ exports.addDonation = function (req, res) {
   res.send();
 };
 
-/**
- *  Show profile
- */
+// Show profile
 exports.show = function (req, res) {
-  var user = req.profile;
+  const user = req.profile;
 
   res.render('users/show', {
     title: user.name,
-    user: user
+    user
   });
 };
 
-/**
- * Send User
- */
+// Send User
+
 exports.me = function (req, res) {
   res.jsonp(req.user || null);
 };
 
-/**
- * Find user by id
- */
+// Find user by id
+
 exports.user = function (req, res, next, id) {
-  User
-    .findOne({
-      _id: id
-    })
+  User.findOne({ _id: id })
     .exec(function (err, user) {
       if (err) return next(err);
       if (!user) return next(new Error('Failed to load User ' + id));
@@ -188,49 +170,56 @@ exports.user = function (req, res, next, id) {
     });
 };
 
-exports.loginWithPassword = function (req, res) {
+exports.loginWithEmail = function (req, res) {
   // get the user credentials from form  req.body.password
   // req.body.email
   User
     .findOne({ email: req.body.email })
-    .then(user => {
-      //compare the password
+    .then((user) => {
       if (!user) {
-        res.json({ "success": false, "message": "authentication failed" });
+        return res.status(404).json({ message: 'User not found' });
       }
-      else {
-        if (user.password != req.body.password) {
-          res.json({ "success": false, "message": "authentication failed" });
-        }
-        else {
-          var token = jwt.sign(user, app.get('my_secret'), {
-            expiresInMinutes: 1400
-          });
-          res.json({
-            "success": true,
-            "token": token
-          })
-        }
+
+      if (!user.authenticate(req.body.password)) {
+        return res.status(403).json({
+          message: 'Invalid password'
+        });
       }
-    })
 
+      const token = jwt.sign(user._id, secret, {
+        expiresIn: '24h'
+      });
 
-  // use the email find user user.finOne({email})
-  //  compare the passwords 
-  // if fails return res that user has invalind password
-  // createToken based on user id {_id: nkakjnajd87iyiuweyqiqoy}
-
-  // send back response {user and token}
-
-
+      user.password = null;
+      res.status(200).json(Object.assign({}, user._doc, { token }));
+    });
 };
 
-exports.AuthoriseUser = function (req, res, next) {
-  //  get the token from headers [x-access-token][muse-access-token]
-  // decrtypt the token
-  // if fails res that invalid token
-  // use the id from the decrypted token
-  // find the use with this id from database user.findById(id)
-  // add the decoded object to req (req.decoded = user)
-  // call next()
-}
+// use the email find user user.finOne({email})
+//  compare the passwords
+// if fails return res that user has invalind password
+// createToken based on user id {_id: nkakjnajd87iyiuweyqiqoy}
+
+// send back response {user and token}
+
+// exports.AuthoriseUser = function (req, res, next) {
+//   //  get the token from headers [x-access-token][muse-access-token]
+//   // decrtypt the token
+//   // if fails res that invalid token
+//   // use the id from the decrypted token
+//   // find the use with this id from database user.findById(id)
+//   // add the decoded object to req (req.decoded = user)
+//   // call next()
+// };
+// exports.signupWithEmail = function (req, res) {
+//   User
+//     .findOne({ email: req.body.email })
+//     .then(user => {
+//       //compare the password
+//       if (!user) {
+//         res.json({ success: false, message: 'authentication failed' });
+//       }
+//       else {
+
+
+//       }
--- config/routes.js
+++ config/routes.js
@@ -7,95 +7,139 @@ const answers = require('../app/controllers/answers');
 const questions = require('../app/controllers/questions');
 const avatars = require('../app/controllers/avatars');
 const users = require('../app/controllers/users');
+const mongoose = require('mongoose'),
+  User = mongoose.model('User');
+// const dev = require('../config/development');
+const port = process.env.PORT || 3000;
 
 module.exports = function (app, passport, auth) {
-    //User Routes
-    app.get('/signin', users.signin);
-    app.get('/signup', users.signup);
-    app.get('/chooseavatars', users.checkAvatar);
-    app.get('/signout', users.signout);
+  //User Routes
+  app.get('/signin', users.signin);
+  app.get('/signup', users.signup);
+  app.get('/chooseavatars', users.checkAvatar);
+  app.get('/signout', users.signout);
 
-    //Setting up the users api
-    app.post('/users', users.create);
-    app.post('/users/avatars', users.avatars);
+  //Setting up the users api
+  app.post('/users', users.create);
+  app.post('/users/avatars', users.avatars);
 
-    // Donation Routes
-    app.post('/donations', users.addDonation);
+  // Donation Routes
+  app.post('/donations', users.addDonation);
 
-    app.post('/users/session', passport.authenticate('local', {
-        failureRedirect: '/signin',
-        failureFlash: 'Invalid email or password.'
-    }), users.session);
+  app.post('/users/session', passport.authenticate('local', {
+    failureRedirect: '/signin',
+    failureFlash: 'Invalid email or password.'
+  }), users.session);
 
-    app.get('/users/me', users.me);
-    app.get('/users/:userId', users.show);
+  app.get('/users/me', users.me);
+  app.get('/users/:userId', users.show);
 
-    //Setting the facebook oauth routes
-    app.get('/auth/facebook', passport.authenticate('facebook', {
-        scope: ['email'],
-        failureRedirect: '/signin'
-    }), users.signin);
+  //Setting the facebook oauth routes
+  app.get('/auth/facebook', passport.authenticate('facebook', {
+    scope: ['email'],
+    failureRedirect: '/signin'
+  }), users.signin);
 
-    app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    failureRedirect: '/signin'
+  }), users.authCallback);
 
-    //Setting the github oauth routes
-    app.get('/auth/github', passport.authenticate('github', {
-        failureRedirect: '/signin'
-    }), users.signin);
+  //Setting the github oauth routes
+  app.get('/auth/github', passport.authenticate('github', {
+    failureRedirect: '/signin'
+  }), users.signin);
 
-    app.get('/auth/github/callback', passport.authenticate('github', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+  app.get('/auth/github/callback', passport.authenticate('github', {
+    failureRedirect: '/signin'
+  }), users.authCallback);
 
-    //Setting the twitter oauth routes
-    app.get('/auth/twitter', passport.authenticate('twitter', {
-        failureRedirect: '/signin'
-    }), users.signin);
+  //Setting the twitter oauth routes
+  app.get('/auth/twitter', passport.authenticate('twitter', {
+    failureRedirect: '/signin'
+  }), users.signin);
 
-    app.get('/auth/twitter/callback', passport.authenticate('twitter', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+  app.get('/auth/twitter/callback', passport.authenticate('twitter', {
+    failureRedirect: '/signin'
+  }), users.authCallback);
 
-    //Setting the google oauth routes
-    app.get('/auth/google', passport.authenticate('google', {
-        failureRedirect: '/signin',
-        scope: [
-            'https://www.googleapis.com/auth/userinfo.profile',
-            'https://www.googleapis.com/auth/userinfo.email'
-        ]
-    }), users.signin);
+  //Setting the google oauth routes
+  app.get('/auth/google', passport.authenticate('google', {
+    failureRedirect: '/signin',
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email'
+    ]
+  }), users.signin);
 
-    app.get('/auth/google/callback', passport.authenticate('google', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+  app.get('/auth/google/callback', passport.authenticate('google', {
+    failureRedirect: '/signin'
+  }), users.authCallback);
 
-    //Finish with setting up the userId param
-    app.param('userId', users.user);
+  //Finish with setting up the userId param
+  app.param('userId', users.user);
 
 
-    app.get('/answers', answers.all);
-    app.get('/answers/:answerId', answers.show);
-    // Finish with setting up the answerId param
-    app.param('answerId', answers.answer);
+  app.get('/answers', answers.all);
+  app.get('/answers/:answerId', answers.show);
+  // Finish with setting up the answerId param
+  app.param('answerId', answers.answer);
 
-    // Question Routes
+  // Question Routes
 
-    app.get('/questions', questions.all);
-    app.get('/questions/:questionId', questions.show);
-    // Finish with setting up the questionId param
-    app.param('questionId', questions.question);
+  app.get('/questions', questions.all);
+  app.get('/questions/:questionId', questions.show);
+  // Finish with setting up the questionId param
+  app.param('questionId', questions.question);
 
-    app.get('/avatars', avatars.allJSON);
+  app.get('/avatars', avatars.allJSON);
 
-    //Home route
+  //Home route
 
-    app.get('/play', index.play);
-    app.get('/', index.render);
+  app.get('/play', index.play);
+  app.get('/', index.render);
 
-    app.post('/api/users/login', users.loginWithEmail)
+  // app.get('/show', function (req, res) {
+  //   res.send('Hello! The API is at http://localhost:' + port + '/api');
+  // });
 
+
+  // const apiRoute = express.Router();
+
+  // apiRoute.get('/xxx', function (req, res) {
+  //   res.json({ message: 'Welcome to the coolest API on earth!' });
+  // });
+
+
+  app.post('/api/authenticate', users.loginWithEmail);
+  // apiRoute.post('/authenticate', function (res, req) {
+  //   //   // exports.loginWithEmail = function (req, res) {
+  //   //   //   get the user credentials from form  req.body.password
+  //   //   //    req.body.email
+  //   //   //   console.log(req.body);
+  // User
+  //   .findOne({
+  //     email: req.body.email
+  //   }, function (user) {
+  //     // compare the password
+  //     if (!user) {
+  //       res.json({ success: false, message: 'authentication failed' });
+  //     } else {
+  //       if (user.authenticate(req.body.password)) {
+  //         console.log('nate');
+
+  //         var token = jwt.sign(user, app.get('my_secret'), {
+  //           expiresInMinutes: 1400
+  //         });
+  //         console.log('waiyaki');
+  //         res.json({
+  //           success: true,
+  //           token
+  //         });
+  //       } else {
+  //         res.json({ success: false, message: 'authentication failed' });
+  //       }
+  //     }
+  //   });
+  // });
+  // app.use('/api', apiRoute);
 };
-
-
