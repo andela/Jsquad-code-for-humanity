c6977e9b1a888f8365fc22b3a7bb52d11e36c1d2
Natalie <natalie.elizabeth@andela.com>
2017-05-15 15:28:40 +0300

    <feature>143410139 ui design and facebook authentication
    Signed-off-by: Natalie <natalie.elizabeth@andela.com>

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
+const currentEnv = 'NODE_ENV' in process.env ? process.env.NODE_ENV : 'development';
 const all = require(__dirname + '/../config/env/all.js');
-const configJson = require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.json') || {};
+const configJson = require(__dirname + '/../config/env/' + currentEnv + '.json') || {};
 
 module.exports = Object.assign(all, configJson);
