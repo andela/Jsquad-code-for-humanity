c69e28fa72d75ccf631902cd4630a4f4b28501b9
JULIE WANJA MUGIRA <julie.mugira@andela.com>
2017-05-17 16:47:20 +0300

    Firebase configuration
    Signed-off-by: JULIE WANJA MUGIRA <julie.mugira@andela.com>

--- config/env/all.js
+++ config/env/all.js
@@ -1,9 +1,14 @@
-var path = require('path'),
-  rootPath = path.normalize(__dirname + '/../..');
-var keys = rootPath + '/keys.txt';
+const path = require('path'),
+  rootPath = path.normalize(`${__dirname}/../..`);
 
 module.exports = {
   root: rootPath,
   port: process.env.PORT || 9000,
   db: process.env.DATABASE_URL,
+  firebase_apiKey: process.env.FIREBASE_APIKEY,
+  firebase_authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  firebase_databaseUrl: process.env.FIREBASE_DATABASEURL,
+  firebase_projectId: process.env.FIREBASE_PROJECTID,
+  firebase_storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  firebase_messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID
 };
